1d9aaea789504c19dceb24ab4699bbff25963955
Devharri <devharri@outlook.com>
2022-03-15 22:00:02 +0200

    Program finds starting point

--- assignment.py
+++ assignment.py
@@ -1,15 +1,7 @@
-# Define imports here
-import helperfunctions                  # Helper functions
-
-#Define global variables here
-startPointY = 0
-startPointX = 0
-coordinates = [0,0]
-mapCode = 1778324
-
 #########################################################################################################
 #                                                                                                       #
 #                                   Junior-developer-assignment                                         #
+#                         https://gofore.com/junior-developer-assignment/                               #
 #                                                                                                       #
 #########################################################################################################
 # Count how many steps it takes to reach (E)                                                            #
@@ -21,7 +13,17 @@ mapCode = 1778324
 #########################################################################################################
 #MAP_CODE 1778324
 
-thislist = ["##################################################", 
+# Define imports here
+import helperfunctions                  # Helper functions
+
+#Define global variables here
+startPointY = 0                         # start point y-coordinate
+startPointX = 0                         # start point x-coordinate
+coordinates = [0,0]                     # start point coordinates as a list                  
+steps = 0                               # step count
+direction = 0                           # stepping direction 0 = up, 1 = right, 2 = down, 3 = left
+
+thislist = ["##################################################", # Copy assignment from a web to a list
 "#................................................#",
 "#.............#..........#.......................#",
 "#................#.........................#.....#",
@@ -57,3 +59,20 @@ coordinates = helperfunctions.searchForStart(thislist)
 startPointY = coordinates[0]
 startPointX = coordinates[1]
 
+print("Start point is: y=" + str(startPointY) + ", x=" + str(startPointX))
+
+while True:
+    if direction == 0:
+        
+        break
+    elif direction == 1:
+        
+        break
+    elif direction == 2:
+        
+        break
+    elif direction == 3:
+        
+        break
+
+print("Total step count is: " + str(steps))
