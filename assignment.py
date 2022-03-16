b11a5bd68724072288b30d6725a3165f507b1cb2
devharri <devharri@outlook.com>
2022-03-16 13:28:44 +0200

    all directions are counting steps

--- assignment.py
+++ assignment.py
@@ -67,24 +67,61 @@ coordY = startPointY
 coordX = startPointX
 
 while True:
+    # direction 0 is up. Y-1, X
     if direction == 0:
+        # check if next character is 'E', if it is, count step by one and break
         if thislist[coordY-1][coordX] == 'E':
             steps += 1
             break
+        # check if next character is '.', if it is, count step by one and substract one from coordinate Y (move up)
         elif thislist[coordY-1][coordX] == '.':
             steps += 1
             coordY -= 1
+        # check if next character is '#', if it is, change direction, do not count a step yet
         elif thislist[coordY-1][coordX] == '#':
             direction = 1
             continue
+    # direction 1 is right. Y, X+1
     elif direction == 1:
-        
-        break
+        # check if next character is 'E', if it is, count step by one and break
+        if thislist[coordY][coordX+1] == 'E':
+            steps += 1
+            break
+        # check if next character is '.', if it is, count step by one and add one to coordinate X (move right)
+        elif thislist[coordY][coordX+1] == '.':
+            steps += 1
+            coordX += 1
+        # check if next character is '#', if it is, change direction, do not count a step yet
+        elif thislist[coordY][coordX+1] == '#':
+            direction = 2
+            continue
+    # direction 2 is down. Y+1, X
     elif direction == 2:
-        
-        break
+        # check if next character is 'E', if it is, count step by one and break
+        if thislist[coordY+1][coordX] == 'E':
+            steps += 1
+            break
+        # check if next character is '.', if it is, count step by one and add one to coordinate Y (move down)
+        elif thislist[coordY+1][coordX] == '.':
+            steps += 1
+            coordY += 1
+        # check if next character is '#', if it is, change direction, do not count a step yet
+        elif thislist[coordY+1][coordX] == '#':
+            direction = 3
+            continue
+    # direction 3 is left. Y, X-1
     elif direction == 3:
-        
-        break
+        # check if next character is 'E', if it is, count step by one and break
+        if thislist[coordY][coordX-1] == 'E':
+            steps += 1
+            break
+        # check if next character is '.', if it is, count step by one and substract one from coordinate X (move left)
+        elif thislist[coordY][coordX-1] == '.':
+            steps += 1
+            coordX -= 1
+        # check if next character is '#', if it is, change direction, do not count a step yet
+        elif thislist[coordY][coordX-1] == '#':
+            direction = 0
+            continue
 
 print("Total step count is: " + str(steps))
